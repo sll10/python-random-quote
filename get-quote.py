dbcf9fc68b1a65429971bedbcb6aeeb18d336384
sll10 <75853917+sll10@users.noreply.github.com>
2020-12-11 13:06:14 -0600

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,3 +1,4 @@
+import random
 def primary():
   print("Keep it logically awesome.")
 
@@ -5,7 +6,9 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-  print(quotes[10])
+ last = 13
+  rnd = random.randint(0, last) 
+print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
